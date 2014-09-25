6395642371ff6fb08b5a2bc5a169be60646e7a0a
Wofho <ltemelakiev@gmail.com>
2014-09-25 02:12:55 -0700

    Create UpperString.java

---
+++ 02/UpperString.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class UpperCase {
+	public static void main(String[] args){
+		String result;
+		String input;
+		
+        Scanner inputReader = new Scanner(System.in); 
+        input = inputReader.nextLine();
+		result=input.toUpperCase();
+		
+		System.out.println(result);
+		
+		
+	}
+}
